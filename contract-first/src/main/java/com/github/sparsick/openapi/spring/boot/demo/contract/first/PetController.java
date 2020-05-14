629a8890bab5b5f94c6483b9693f8ea88c2fede7
sparsick <sparsick@web.de>
2020-05-14 11:52:29 +0200

    update api spec to openapi 3.0

--- contract-first/src/main/java/com/github/sparsick/openapi/spring/boot/demo/contract/first/PetController.java
+++ contract-first/src/main/java/com/github/sparsick/openapi/spring/boot/demo/contract/first/PetController.java
@@ -1,18 +1,15 @@
 package com.github.sparsick.openapi.spring.boot.demo.contract.first;
 
-import de.github.sparsick.openapi.spring.boot.contract.first.api.PetApi;
-import de.github.sparsick.openapi.spring.boot.contract.first.model.Pet;
+import de.github.sparsick.openapi.spring.boot.contract.first.api.PetsApi;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.validation.Valid;
-
 @RestController
-public class PetController implements PetApi {
+public class PetController implements PetsApi {
 
     @Override
-    public ResponseEntity<Void> addPet(@Valid Pet body) {
+    public ResponseEntity<Void> createPets() {
         return new ResponseEntity<>(HttpStatus.OK);
     }
 }
